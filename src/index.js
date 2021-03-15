7e663cb23d8313a3fd569cf5d562dc859ab46b4b
sashabely221100 <sashabelyy2012@gmail.com>
2021-03-15 03:01:36 +0300

    init: start typical-arrays-problems task feat: implement functions min, max & avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return (array === undefined || array.length === 0) ? 0 : Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+
+
+exports.max = function max(array) {
+
+
+    return (array === undefined || array.length === 0) ? 0 : Math.max(...array);
+
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    let sum = array.reduce(function(tmpsum, curVal) {
+        return tmpsum + curVal;
+    });
+    return sum / array.length;
 }
